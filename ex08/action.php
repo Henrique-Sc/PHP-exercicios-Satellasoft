34ed40aae8989fd411ffeb449f52cee6f694afc7
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-03 09:28:23 -0300

    modificando o nome dos arquivos

--- ex08/action.php
+++ ex08/action.php
@@ -3,33 +3,40 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Resultado - Exercício 08</title>
+	<title>Resultado - Exercício 07</title>
 
 	<style type="text/css">
 		body {
 			font-family: arial;
 		}
 
-
 	</style>
 </head>
 <body>
-	<h2>Resultado exercício 08 - Maioridade</h2>
+	<h2>Resultado exercício 07 - Média</h2>
 
-	<?php
+	<?php 
 
-	$nome = ucwords($_POST['nome']);
-	$idade = $_POST['idade'];
-	
-	echo "$nome, você é <b>";
-	if ($idade < 18) {
-		echo "menor de idade!</b>";
-	}
+		$notas = [$_POST['nota1'], $_POST['nota2'], $_POST['nota3']];
+		$media = number_format(array_sum($notas) / count($notas), 2);
 
-	else {
-		echo "maior de idade!</b>";
-	}
+		echo "Notas: <br>";
+		$c = 1;
+		foreach ($notas as $key => $value) {
+			echo " - Nota $c: <b>$value</b> <br>";
+			$c++;
+		}
+		echo "<br>Média: <b>$media</b><br>";
+		if ($media < 5) {
+			echo "Reprovado.";
+		}
 
+		elseif ($media >= 5 and $media < 7) {
+			echo "Recuperação.";
+		}
+
+		else {
+			echo "Aprovado.";
+		}
 	?>
-</body>
 </html>
